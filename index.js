945b77e025d6c17bede6d33d8c77adee4a0b473f
defselom <selomadzomla@gmail.com>
2025-05-14 22:28:56 +0000

    feat(whoami) : get lang , ip and user agent agent

--- index.js
+++ index.js
@@ -5,12 +5,10 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -23,6 +21,14 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get("/api/whoami", function (req, res) {
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress: req.ip, language, software })
+})
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
